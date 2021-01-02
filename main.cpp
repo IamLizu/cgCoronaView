df0a5427c68a6c1e5a07fd86a34adaa6ea32cb2f
AshRahman <54446710+AshRahman@users.noreply.github.com>
2021-01-03 01:05:04 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -911,7 +911,6 @@ void controlsScreen(){
     glColor3f(0.596, 0.984, 0.596);
     drawstring(250.0,700.0,0.0,"INSTRUCTIONS");
     glColor3f(1.000, 0.980, 0.941);
-
     drawstring(150.0,640.0,0.0,"PRESS 'D'");
     glColor3f(1.000, 0.980, 0.941);
     drawstring(300.0,640.0,0.0,"DAY MODE");
@@ -922,7 +921,7 @@ void controlsScreen(){
     glColor3f(1.000, 0.980, 0.941);
     drawstring(150.0,600.0,0.0,"PRESS 'N'");
     glColor3f(1.000, 0.980, 0.941);
-    drawstring(300.0,560.0,0.0,"CAR MOVING");
+    drawstring(300.0,560.0,0.0,"CAR APPEAR");
     glColor3f(1.000, 0.980, 0.941);
     drawstring(150.0,560.0,0.0,"PRESS 'C'");
     glColor3f(1.000, 0.980, 0.941);
@@ -946,10 +945,9 @@ void controlsScreen(){
     glColor3f(1.000, 0.980, 0.941);
     drawstring(150.0,360.0,0.0,"PRESS '.'");
     glColor3f(1.000, 0.980, 0.941);
-    drawstring(300.0,320.0,0.0,"CAR SPEED RESTORE");
+    drawstring(300.0,320.0,0.0,"CAR MOVE");
     glColor3f(1.000, 0.980, 0.941);
     drawstring(150.0,320.0,0.0,"PRESS ','");
-
     drawstring(300.0,280.0,0.0,"CAR DISSAPEAR");
     glColor3f(1.000, 0.980, 0.941);
     drawstring(150.0,280.0,0.0,"PRESS 'V'");
@@ -960,7 +958,6 @@ void controlsScreen(){
     drawstring(300.0,200.0,0.0,"ClOSE PROJECT");
     glColor3f(1.000, 0.980, 0.941);
     drawstring(150.0,200.0,0.0,"PRESS 'ESC'");
-
     glColor3f(0.596, 0.984, 0.596);
     drawstring(250.0,150.0,0.0,"NOW PRESS ENTER");
     glFlush();
